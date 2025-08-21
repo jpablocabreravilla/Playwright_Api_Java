ca8c6374f36cefb71682ed157fa39ecdb7c6cdb0
jpablocabreravilla <jpablocabreravilla@gmail.com>
2025-08-20 22:30:08 -0500

    Animal and products Tests

--- src/test/java/tests/AnimalTests.java
+++ src/test/java/tests/AnimalTests.java
@@ -65,4 +65,80 @@ public class AnimalTests extends BaseTest {
         Assertions.assertEquals(200, response.status());
     }
 
+    @Test
+    public void crearAnimalTest(APIRequestContext request) {
+        final var requestBody = """
+                {
+                    "nombre": "Nova",
+                    "tipo": "cautivo",
+                    "edad": 3,
+                    "peso": 42.3,
+                    "genero": "hermafrodita",
+                    "amo": {
+                        "nombre": "Dovie",
+                        "apellido": "McLaughlin",
+                        "correo": "Dovie_McLaughlin42@hotmail.com",
+                        "edad": 52,
+                        "pais": "Paraguay"
+                    }
+                }
+                """;
+
+        requestOptions.setData(requestBody);
+        requestOptions.setHeader("Content-Type", "application/json");
+
+        response = request.post("animales", requestOptions);
+        ApiLogger.logApi(response, Method.POST);
+
+        Assertions.assertEquals(201, response.status());
+    }
+
+    @Test
+    public void actualizarAnimalTest(APIRequestContext request) {
+        final var requestBody = """
+                {
+                    "nombre": "Curso en Vivo",
+                    "tipo": "domestico",
+                    "edad": 25,
+                    "peso": 32.00,
+                    "genero": "hembra",
+                    "amo": {
+                        "nombre": "Christophe",
+                        "apellido": "Stanton",
+                        "correo": "Christophe_Stanton52@hotmail.com",
+                        "edad": 30,
+                        "pais": "Guam"
+                    }
+                }
+                """;
+
+        requestOptions.setData(requestBody);
+        requestOptions.setHeader("Content-Type", "application/json");
+
+        response = request.put("animales/5", requestOptions);
+        ApiLogger.logApi(response, Method.PUT);
+
+        Assertions.assertEquals(200, response.status());
+    }
+
+    @Test
+    public void actualizarParcialmenteAnimalTest(APIRequestContext request) {
+        final var requestBody = """
+                {
+                    "nombre": "Hola",
+                    "tipo": "Mundo",
+                    "edad": 25,
+                    "peso": 32.00
+                }
+                """;
+
+        requestOptions.setData(requestBody);
+        requestOptions.setHeader("Content-Type", "application/json");
+
+        response = request.patch("animales/5", requestOptions);
+        ApiLogger.logApi(response, Method.PATCH);
+
+        Assertions.assertEquals(200, response.status());
+    }
+
 }
--- src/test/java/tests/ProductoTests.java
+++ src/test/java/tests/ProductoTests.java
@@ -64,4 +64,120 @@ public class ProductoTests extends BaseTest {
         Assertions.assertEquals(200, response.status());
     }
 
+    @Test
+    public void crearProductoTest(APIRequestContext request) {
+        final var requestBody = """
+        {
+            "nombre": "Blass",
+            "precio": 18.775,
+            "cantidad": 9,
+            "peso": 9.648,
+            "perecible": true,
+            "volumen": 42.883,
+            "criticas": [
+                {
+                    "puntaje": 3.79,
+                    "comentario": "Tenderly braised lamb in a rich sesame seed and brussels sprouts sauce",
+                    "correo": "Geraldine.Homenick91@yahoo.com",
+                    "usuario": "Geraldine_Homenick29"
+                },
+                {
+                    "puntaje": 2.24,
+                    "comentario": "Fresh and crispy salad with homemade dressing",
+                    "correo": "Joanne.Larson92@hotmail.com",
+                    "usuario": "Joanne_Larson92"
+                }
+            ],
+            "etiquetas": [
+                "Soft",
+                "Intelligent",
+                "Elegant",
+                "Electronic",
+                "Incredible",
+                "Small",
+                "Handcrafted",
+                "Licensed",
+                "Oriental"
+            ]
+        }
+        """;
+
+        requestOptions.setData(requestBody);
+        requestOptions.setHeader("Content-Type", "application/json");
+
+        response = request.post("productos", requestOptions);
+        ApiLogger.logApi(response, Method.POST);
+
+        Assertions.assertEquals(201, response.status());
+    }
+
+    @Test
+    public void actualizarProductoTest(APIRequestContext request) {
+        final var requestBody = """
+        {
+            "nombre": "Blass Vivo 2",
+            "precio": 39.99,
+            "cantidad": 9,
+            "peso": 9.648,
+            "perecible": true,
+            "volumen": 42.883,
+            "criticas": [
+                {
+                    "puntaje": 3.79,
+                    "comentario": "Tenderly braised lamb in a rich sesame seed and brussels sprouts sauce",
+                    "correo": "Geraldine.Homenick91@yahoo.com",
+                    "usuario": "Geraldine_Homenick29"
+                },
+                {
+                    "puntaje": 2.24,
+                    "comentario": "Fresh and crispy salad with homemade dressing",
+                    "correo": "Joanne.Larson92@hotmail.com",
+                    "usuario": "Joanne_Larson92"
+                }
+            ],
+            "etiquetas": [
+                "Soft",
+                "Intelligent",
+                "Elegant",
+                "Electronic",
+                "Incredible",
+                "Small",
+                "Handcrafted",
+                "Licensed",
+                "Oriental"
+            ]
+        }
+        """;
+
+        requestOptions.setData(requestBody);
+        requestOptions.setHeader("Content-Type", "application/json");
+
+        response = request.put("productos/2", requestOptions);
+        ApiLogger.logApi(response, Method.PUT);
+
+        Assertions.assertEquals(200, response.status());
+    }
+
+    @Test
+    public void actualizarParcialmenteProductoTest(APIRequestContext request) {
+        final var requestBody = """
+        {
+            "nombre": "Blass",
+            "precio": 4.892,
+            "cantidad": 3,
+            "peso": 3.71,
+            "perecible": false,
+            "volumen": 58.396
+        }
+        """;
+
+        requestOptions.setData(requestBody);
+        requestOptions.setHeader("Content-Type", "application/json");
+
+        response = request.patch("productos/2", requestOptions);
+        ApiLogger.logApi(response, Method.PATCH);
+
+        Assertions.assertEquals(200, response.status());
+    }
+
 }
